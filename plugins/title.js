bee7777d278643360868fea0befb35e8ff4d28f4
45aff0062cf1b0e55a6532b0a513db6be3952d17 <bmclifford@gmail.com>
2016-01-09 02:12:52 +0000

    fix title property name

--- plugins/title.js
+++ plugins/title.js
@@ -1,5 +1,5 @@
 var title = function(ctx, next) {
-  if (!ctx.options.metadata) return next();
+  if (!ctx.options.title) return next();
   //if (ctx.options.playlist.length > 1) return next();
 
   ctx.options.playlist[0].media = {
